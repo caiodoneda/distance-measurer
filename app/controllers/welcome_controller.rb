3c3e9782074a87f9234e50f0b6b78cc7b6dc1510
Caio Doneda <caiodoneda@gmail.com>
2016-02-24 15:53:48 -0300

    testing google maps api

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,32 @@
+require 'http'
+require 'rubygems'
+require 'json'
+
 class WelcomeController < ApplicationController
   def index
+  	if params[:origin] != nil
+  		origin = params[:origin]
+	  	response = JSON.parse(HTTP.get('https://maps.googleapis.com/maps/api/geocode/json', 
+	  		                      :params => { :address => origin, 'key' => 'AIzaSyBHcfHe8sW-QhqkfhTZygv6nRsSY8zt6xU'}))
+	  	
+	  	origin_geocode = response['results'].last['geometry']['location']
+  	else
+  		origin = ''
+  		origin_geocode = ''
+	end
+
+	if params[:destination] != nil
+  		destination = params[:destination]
+	  	response = JSON.parse(HTTP.get('https://maps.googleapis.com/maps/api/geocode/json', 
+	  		                      :params => { :address => destination, 'key' => 'AIzaSyBHcfHe8sW-QhqkfhTZygv6nRsSY8zt6xU'}))
+	  	destination_geocode = response['results'].last['geometry']['location']
+  	else
+  		destination = ''
+  		destination_geocode = ''	
+  	end
+
+  	puts 
+
+  	@welcome = {origin: origin, origin_geocode: origin_geocode, destination: destination, destination_geocode: destination_geocode}
   end
 end
