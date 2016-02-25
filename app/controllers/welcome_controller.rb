63413cffbdbcd78515550945bd5ea9cbc4038efa
Caio Doneda <caiodoneda@gmail.com>
2016-02-25 14:08:30 -0300

    adding results information

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,23 +7,28 @@ class WelcomeController < ApplicationController
   	map = {:origin => 'Praça XV de Novembro - Rua Tenente Silveira - Centro, Florianópolis - SC',
   		   :destination => 'Shopping Iguatemi, Florianópolis - Santa Monica, State of Santa Catarina'}
   	  	
-  	if params[:origin] != nil
+  	if !params[:origin].blank?
   		origin = params[:origin]
   		map[:origin] = params[:origin]
   	end
 
-  	if params[:destination] != nil
+  	if !params[:destination].blank?
   		destination = params[:destination]
   		map[:destination] = params[:destination]
   	end
     	
-  	driving = HTTP.get('https://maps.googleapis.com/maps/api/distancematrix/json', 
+  	response = HTTP.get('https://maps.googleapis.com/maps/api/distancematrix/json', 
 		      :params => { :origins => map[:origin], 
 		      	           :destinations => map[:destination], 
 		      	           :mode => 'driving',
 		      	           :departure_time => 'now',
 		      	           :key => 'AIzaSyBHcfHe8sW-QhqkfhTZygv6nRsSY8zt6xU'})
-  	
-  	@welcome = {origin: origin, destination: destination, driving: JSON.parse(driving), map: map}
+
+  	response = JSON.parse(response)
+  	elements = response['rows'].first['elements'].first
+  	driving = {:distance => elements['distance']['value'],
+  			   :duration => elements['duration_in_traffic']['value']}
+
+  	@welcome = {origin: origin, destination: destination, driving: driving, map: map}
   end
 end
