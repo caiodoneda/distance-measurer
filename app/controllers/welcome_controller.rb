18bea11e79808d0d473be3d77666303fc2b480ae
Caio Doneda <caiodoneda@gmail.com>
2016-02-25 12:56:38 -0300

    adding map with routes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,32 +1,29 @@
 require 'http'
-require 'rubygems'
 require 'json'
 
 class WelcomeController < ApplicationController
   def index
+    driving = ''
+  	map = {:origin => 'Praça XV de Novembro - Rua Tenente Silveira - Centro, Florianópolis - SC',
+  		   :destination => 'Shopping Iguatemi, Florianópolis - Santa Monica, State of Santa Catarina'}
+  	  	
   	if params[:origin] != nil
   		origin = params[:origin]
-	  	response = JSON.parse(HTTP.get('https://maps.googleapis.com/maps/api/geocode/json', 
-	  		                      :params => { :address => origin, 'key' => 'AIzaSyBHcfHe8sW-QhqkfhTZygv6nRsSY8zt6xU'}))
-	  	
-	  	origin_geocode = response['results'].last['geometry']['location']
-  	else
-  		origin = ''
-  		origin_geocode = ''
-	end
-
-	if params[:destination] != nil
-  		destination = params[:destination]
-	  	response = JSON.parse(HTTP.get('https://maps.googleapis.com/maps/api/geocode/json', 
-	  		                      :params => { :address => destination, 'key' => 'AIzaSyBHcfHe8sW-QhqkfhTZygv6nRsSY8zt6xU'}))
-	  	destination_geocode = response['results'].last['geometry']['location']
-  	else
-  		destination = ''
-  		destination_geocode = ''	
+  		map[:origin] = params[:origin]
   	end
 
-  	puts 
-
-  	@welcome = {origin: origin, origin_geocode: origin_geocode, destination: destination, destination_geocode: destination_geocode}
+  	if params[:destination] != nil
+  		destination = params[:destination]
+  		map[:destination] = params[:destination]
+  	end
+    	
+  	driving = HTTP.get('https://maps.googleapis.com/maps/api/distancematrix/json', 
+		      :params => { :origins => map[:origin], 
+		      	           :destinations => map[:destination], 
+		      	           :mode => 'driving',
+		      	           :departure_time => 'now',
+		      	           :key => 'AIzaSyBHcfHe8sW-QhqkfhTZygv6nRsSY8zt6xU'})
+  	
+  	@welcome = {origin: origin, destination: destination, driving: JSON.parse(driving), map: map}
   end
 end
